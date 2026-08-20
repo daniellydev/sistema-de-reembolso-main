7642c8566bdd2b6ce41c2bf419b6d4e741f986c4
Danielly Batista <daniellybsdev@gmail.com>
2026-08-19 23:53:06 -0300

    Add SVG icons and implement expense reimbursement form
    - Added SVG icons for food, logo, others, remove, services, and transport.
    - Created an HTML structure for the reimbursement request form.
    - Implemented JavaScript for input formatting and form submission handling.
    - Styled the form and expense list with CSS for better user experience.

---
+++ scripts.js
@@ -0,0 +1,51 @@
+// Seleciona os elementos do formulário.
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
+
+// Captura o evento de input para formatar o valor.
+amount.oninput = () => {
+    // Obtém o valor atual do input e remove os caracteres não númericos
+    let value = amount.value.replace(/\D/g, "")
+
+    // Transforma o valor em centavos.
+    value = Number(value) / 100
+
+    // Atualiza o valor do input.
+    amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value){
+    // Formata o valor no padrão BRL (Real Brasileiro)
+    value = value.toLocaleString("pt-BR", {
+        style: "curency",
+        currency:"BRL",
+    })
+
+    return value
+}
+
+// Captura o evento de submit do formlário para obter os valores.
+form.onsubmit = (event) => {
+    // Previne o comportamento padrão de recarregar a página.
+    event.preventDefault()
+    
+    // Cria um objeto com os detalhes na nova despesa.
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedindex].text, // Apenas as opções selecionadas dentro do category, sendo assim não pega todas.
+        amount : amount.value,
+        created_at: new Date()
+    }
+
+    // Chama a função que irá adicionar um item na lista.
+    expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense){
+
+
+}
